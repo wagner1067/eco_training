74132ca8a2771c091c15369a1e3e3617b823c995
wagner1067 <barra-wag@hotmail.com>
2024-07-12 14:06:22 -0300

    add as url no icons

--- eco-training-landing-page/src/components/section/Header.js
+++ eco-training-landing-page/src/components/section/Header.js
@@ -11,13 +11,13 @@ function Header() {
                 <img src={Logo} alt="Logo Eco Training" />
             </div>
             <ul className={style.links}>
-                <li><Nav.Link href="#about">Sobre</Nav.Link></li>
+                <li><Nav.Link href='#about'>Sobre</Nav.Link></li>
                 <li><Nav.Link href="#contact">Contate-nos</Nav.Link></li>
             </ul>
             <ul className={style.social_icons}>
-                <li><a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebookSquare /></a></li>
-                <li><a href="https://www.whatsapp.com" target="_blank" rel="noopener noreferrer"><FaWhatsappSquare /></a></li>
-                <li><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></li>
+                <li><a href="https://www.facebook.com/simuladordeescada" target="_blank" rel="noopener noreferrer"><FaFacebookSquare /></a></li>
+                <li><a href="https://api.whatsapp.com/send/?phone=554535227220&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer"><FaWhatsappSquare /></a></li>
+                <li><a href="https://www.instagram.com/simulador_de_escada" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></li>
             </ul>
         </div>
     );
